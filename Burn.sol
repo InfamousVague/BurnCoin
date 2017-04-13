6a730b14df4a7959d954ee034da8f5d5c7d2999b
Matt Wisniewski <contact.mattdylan@gmail.com>
2017-04-12 22:39:51 -0400

    Update Burn.sol

--- Burn.sol
+++ Burn.sol
@@ -1,33 +1,31 @@
 pragma solidity ^0.4.0;
 
 // Burn coin burns a little bit of coin on every transaction gradually recuding
-// Circulation amount.
+// the total supply
 contract Burn {
-    // Contract address
+    // Constants
     address public minter;
-    
-    // Inital supply
     uint public supply;
-    
-    // Total coin burned to date
-    uint public burned = 0;
-
     string public name;
     string public symbol;
     uint public decimals;
-    
-    // Burn rate is x ppt 
     uint public burnRate;
 
+    // Variables
+    uint public burned = 0;
     mapping (address => uint) public balances;
     
-    // Triggered on transactions
+    // Events
     event Sent(address from, address to, uint amount);
-    
-    // Triggered when ember is burned
     event Burned(address burner, uint amount);
     
-    // Assign the minter, and send them the inital supply
+    /* @constructor Burn
+     * @param {uint} _supply - The total coin in circulation
+     * @param {string} _name - The name of the coin
+     * @param {string} _symbol - The symbol of the coin
+     * @param {uint} _decimals - The total decimal places
+     * @param {uint} _burnRate - ppt to burn
+     */
     function Burn(uint _supply, string _name, string _symbol, uint _decimals, uint _burnRate) {
         minter = msg.sender;
         supply = _supply;
@@ -38,7 +36,11 @@ contract Burn {
         balances[minter] = _supply;
     }
     
-    // Burns off a little bit of coin
+    /* @function burnCoin
+     * @name burnCoin
+     * @param {uint} amount - Amount of coin to burn from
+     * @returns {uint} remaining - Amount of coin left 
+     */
     function burnCoin(uint amount) internal returns (uint remaining) {
         uint burn = (amount / 1000) * burnRate;
         burned += burn;
@@ -47,21 +49,17 @@ contract Burn {
         return (amount - burn);
     }
 
+    /* @function send
+     * @name send
+     * @param {address} receiver - Address to send coin to
+     * @param {uint} amount - Amount of coin to send
+     */
     function send(address receiver, uint amount) public {
-        // If the sender doesn't have enough coin throw
         if (balances[msg.sender] < amount) throw;
-        // If the transaction is too low to burn coin throw
         if (amount < burnRate * 1000) throw;
-        
-        // Sender is deducted total amount
         balances[msg.sender] -= amount;
-        
         uint remining = burnCoin(amount);
-        
-        // Receiver receives token after burn
         balances[receiver] += remining;
-        
-        // Trigger a transaction event
         Sent(msg.sender, receiver, remining);
     }
 }
