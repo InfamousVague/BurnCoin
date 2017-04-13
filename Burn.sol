aae49cf81d0ae2aef9fe64e12710d1673ad2410e
wski <contact.mattdylan@gmail.com>
2017-04-13 01:02:33 -0400

    feat(ICO): Added semi-complete ICO functionality

--- Burn.sol
+++ Burn.sol
@@ -1,25 +1,57 @@
-pragma solidity ^0.4.0;
+pragma solidity ^0.4.2;
+
+contract owned {
+    address public owner;                      // Owner of contract
+
+    function owned() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner {
+        if (msg.sender != owner) throw;
+        _;
+    }
+
+    function transferOwnership(address newOwner) onlyOwner {
+        owner = newOwner;
+    }
+}
 
 // Burn coin burns a little bit of coin on every transaction gradually recuding
 // the total supply
-contract Burn {
+contract Burn is owned {
     // Constants
-    string public standard = 'Token 0.1';
-    address public minter;
-    uint public supply;
-    string public name;
-    string public symbol;
-    uint public decimals;
-    uint public burnRate;
-
+    string public standard = 'Token 0.1';           // Token standard
+    uint public supply = 10000000000000000000000000;// Total Supply
+    string public name = 'Burn';                    // Name of the token
+    string public symbol = 'BURN';                  // Symbol of the token
+    uint public decimals = 18;                      // Decimals for token
+    uint public burnRate = 1;//ppt                  // burnRate where x ppt is burned
+    
     // Variables
-    uint public burned = 0;
-    mapping (address => uint) public balances;
+    uint public burned = 0;                         // Total token burned
+    mapping (address => uint) public balances;      // Balances
+
+    // ICO Constants
+    bool public ICOActive = true;                   // Dignifies if ICO funding is still active
+    uint public exchangeRate = 500;                 // ICO return is equal to 1 wei * exchange rate
+
+    // ICO Variables
+    uint public ICOCirculation = 0;                 // Used to ensure ICO offerings do not exceed supply
+    mapping (address => uint) public ICObalances;   // ICO balances, to be transferred to actual balances upon ICO colsing
     
     // Events
     event Transfer(address from, address to, uint amount);
     event Burned(address burner, uint amount);
-    
+
+    /* @modifier onlyOwner
+     * @description ensures only the contract owner can execute this command
+     */
+    modifier onlyOwner {
+        if (msg.sender != owner) throw;
+        _;
+    }
+
     /* @constructor Burn
      * @param {uint} _supply - The total coin in circulation
      * @param {string} _name - The name of the coin
@@ -28,13 +60,12 @@ contract Burn {
      * @param {uint} _burnRate - ppt to burn
      */
     function Burn(uint _supply, string _name, string _symbol, uint _decimals, uint _burnRate) {
-        minter = msg.sender;
         supply = _supply;
         name = _name;
         symbol = _symbol;
         decimals = _decimals;
         burnRate = _burnRate;
-        balances[minter] = _supply;
+        balances[owner] = _supply;
     }
     
     /* @function burnCoin
@@ -55,16 +86,51 @@ contract Burn {
      * @param {address} receiver - Address to send coin to
      * @param {uint} amount - Amount of coin to send
      */
-    function transfer(address _to, uint _value) public {
-        if (balances[msg.sender] < _value) throw;
-        if (amount < burnRate * 1000) throw;
-        balances[msg.sender] -= _value;
-        uint remining = burnCoin(_value);
-        balances[_to] += remining;
-        Transfer(msg.sender, _to, remining);
+    function transfer(address _to, uint _value) productionOnly public returns (bool success) {
+        if (balances[msg.sender] < _value) {
+            return false;                                       // Sender does not have enough coin to send
+        } else if (_value < burnRate * 1000) {
+            return false;                                       // Send amount too low
+        } else {
+            balances[msg.sender] -= _value;
+            uint remining = burnCoin(_value);
+            balances[_to] += remining;
+            Transfer(msg.sender, _to, remining);
+            return true;
+        }   
     }
     
-    function () {
-        throw;
+    /* ICO 
+     * During the ICO phase people may trade their Ether for Burn coin, balances will be stored in the 
+     * ICOBalances and transferred over to the production balances once closeICO is executed by the contract
+     * owner. Remaining avalible coins will be sent to the contract owner.
+     */
+    
+    modifier productionOnly () {
+        if (ICOActive) throw;
+        _;
+    }
+
+
+    function closeICO() onlyOwner {
+        balances[owner] = supply - ICOCirculation;              // Send remaining coin to owner
+        ICOActive = false;                                      // Dignify that ICO funding is over
+    }
+
+    function ICOTransfer(address _to, uint _value) public returns (bool success) {
+        if (ICOCirculation < supply) {
+            uint payout = _value * exchangeRate;
+            balances[_to] += payout;
+            ICOCirculation += payout;
+            balances[owner] -= payout;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // Executes when somebody sends ether to contract address
+    function () payable {
+        ICOTransfer(msg.sender, msg.value);
     }
 }
